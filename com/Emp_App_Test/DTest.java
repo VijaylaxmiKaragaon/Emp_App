3937e94c0097f7e1b01cd6dbd50baa1c1ba5779f
Vijaylaxmi Karagaon <vijaylaxmisk04@gmail.com>
2026-05-04 22:55:25 +0530

    Uncomment department management functionality

--- com/Emp_App_Test/DTest.java
+++ com/Emp_App_Test/DTest.java
@@ -5,32 +5,33 @@ public class TestD {
 //		Dept d=new Dept();
 		DeptDAO ddao=new DeptDAOImpl();
         
-//		System.out.println("Enter the dept id:");
-//		d.setDno(sc.nextInt());
-//		
-//		System.out.println("Enter the dept name:");
-//		d.setDname(sc.next());
-//		
-//		System.out.println("Enter the dept location:");
-//		d.setLocation(sc.next());
-//		
-//		ddao.addDept(d);
+		System.out.println("Enter the dept id:");
+		d.setDno(sc.nextInt());
+		
+		System.out.println("Enter the dept name:");
+		d.setDname(sc.next());
+		
+		System.out.println("Enter the dept location:");
+		d.setLocation(sc.next());
+		
+		ddao.addDept(d);
 		
 		//findById
-//		System.out.println("Enter dept id:");
-//		Dept deptl=ddao.findById(sc.nextInt());
-//		System.out.println(deptl);
+		System.out.println("Enter dept id:");
+		Dept deptl=ddao.findById(sc.nextInt());
+		System.out.println(deptl);
 		
 		//findByAll
-//		List<Dept> ls=ddao.findAll();
-//         for(Dept ele : ls) {
-//       	System.out.println(ele);
-//       	}
-		//deleteDept
-//		System.out.println("Enter dept number which you want to delete");
-//		Dept deptl=ddao.deleteDept(sc.nextInt());
-//		System.out.println(deptl);
-//		
+		List<Dept> ls=ddao.findAll();
+        for(Dept ele : ls) {
+      	System.out.println(ele);
+      	}
+		deleteDept
+		System.out.println("Enter dept number which you want to delete");
+		Dept deptl=ddao.deleteDept(sc.nextInt());
+		System.out.println(deptl);
+		
+		//UpdateAll
 		System.out.println("Enter dept id:");
 		Dept d=ddao.findById(sc.nextInt());
 		System.out.println("Before update");
